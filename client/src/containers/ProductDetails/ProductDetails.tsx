caf25513606d8f3615fa8a7186778e9dfc0aba31
Makuza Mugabo Verite <mugaboverite@gmail.com>
2020-09-03 16:15:01 +0200

    Product details page

--- client/src/containers/ProductDetails/ProductDetails.tsx
+++ client/src/containers/ProductDetails/ProductDetails.tsx
@@ -1,23 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import imac from '../../assets/products/imac.png'
+import './ProductDetails.css'
 
 function ProductDetails() {
+	const [quantity, setQuantity] = useState<string>('0')
+
 	return (
-		<section>
-			<div className='product'>
-				<h1>Product photo</h1>
-			</div>
-
-			<div>
-				<p>Product className</p>
-				<p>Product Description</p>
-				<div>
-					<input type='number' name='prod_num' value='1' min='1' max='1000' />
+		<>
+			<section className='product_description'>
+				<div className='product'>
+					<img src={imac} alt='imac' />
 				</div>
-				<button>Add to cart</button>
-			</div>
-
-			<h1>More products </h1>
-		</section>
+				<div className='product_info'>
+					<h1>Product className rating</h1>
+					<p>
+						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi id
+						asperiores iusto itaque nemo inventore qui, doloremque dolorum pariatur
+						harum dignissimos? Quaerat illo repellendus temporibus! Voluptate rem
+						aliquid illum odio.
+					</p>
+					<div>
+						<input
+							type='number'
+							name='prod_num'
+							min='1'
+							max='1000'
+							value={quantity}
+							onChange={(e) => setQuantity(e.target.value)}
+						/>
+					</div>
+					<button>
+						<i className='fas fa-cart-plus' /> Add to cart
+					</button>
+				</div>
+			</section>
+		</>
 	)
 }
 
