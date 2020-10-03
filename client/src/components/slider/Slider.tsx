a361a08b4021f529f7b03d62214b54ba118e3618
Makuza Mugabo Verite <mugaboverite@gmail.com>
2020-10-03 11:13:12 +0200

    setup emotion

--- client/src/components/slider/Slider.tsx
+++ client/src/components/slider/Slider.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { images } from './imageData'
 import { motion } from 'framer-motion'
-import { Swipeable, useSwipeable } from 'react-swipeable'
+import { useSwipeable } from 'react-swipeable'
 
 function Slider() {
   const [position, setPosition] = useState(0)
@@ -27,15 +27,13 @@ function Slider() {
     <div
       style={{
         height: '50vh',
-        width: '100vw',
+        width: '90vw',
         overflow: 'hidden',
-        padding: '0px',
-        margin: '0px',
+        padding: '30px 20px',
+        margin: '0px 30px',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        background:
-          'linear-gradient(180deg, rgb(255, 94, 0), rgb(143, 133, 49))',
       }}
     >
       <div
@@ -46,27 +44,27 @@ function Slider() {
       >
         {images.map((url, index) => (
           <motion.div
-            style={{
-              position: 'absolute',
-              width: '100vw',
-              height: '100vh',
-              overflow: 'hidden',
-              background: 'white',
-              borderRadius: '30px',
-              top: '-45px',
-            }}
-            key={index}
-            initial={{ scale: 0, rotate: -180 }}
-            animate={{
-              rotate: 0,
-              left: `${(index - position) * 60 - 30}vw`,
-              scale: index - position ? 1 : 0.8,
-            }}
-            transition={{
-              type: 'spring',
-              stiffness: 260,
-              damping: 20,
-            }}
+          // style={{
+          //   position: 'absolute',
+          //   width: '100vw',
+          //   height: '100vh',
+          //   overflow: 'hidden',
+          //   background: 'white',
+          //   borderRadius: '30px',
+          //   top: '-45px',
+          // }}
+          // key={index}
+          // initial={{ scale: 0, rotate: -180 }}
+          // animate={{
+          //   rotate: 0,
+          //   left: `${(index - position) * 60 - 30}vw`,
+          //   scale: index - position ? 1 : 0.8,
+          // }}
+          // transition={{
+          //   type: 'spring',
+          //   stiffness: 260,
+          //   damping: 20,
+          // }}
           >
             <img
               src={url}
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,14 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 import storeContainer from './store'
 import { Provider } from 'react-redux'
+import { Global, css } from '@emotion/core'
 
 ReactDOM.render(
   <Provider store={storeContainer}>
     <React.StrictMode>
+      <Global
+        styles={css`
+          @import url('https://fonts.googleapis.com/css2?family=Alata&display=swap');
+
+          :root {
+            --whitish: #f1f3de;
+            --second-whitish: #eb8f8f;
+            --red: #ec0101;
+            --dark-red: #cd0a0a;
+          }
+
+          * {
+            box-sizing: border-box;
+            margin: 0;
+            padding: 0;
+            font-family: 'Alata', sans-serif;
+          }
+          body {
+            background-color: var(--whitish);
+          }
+        `}
+      />
       <App />
     </React.StrictMode>
   </Provider>,
