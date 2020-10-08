3557825cc81b0cf48031afe98f03b0794c5c1e7c
Makuza Mugabo Verite <mugaboverite@gmail.com>
2020-10-08 17:46:24 +0200

    fixed css for home page

---
+++ client/src/Pages/Home.tsx
@@ -0,0 +1,45 @@
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core'
+import { Fragment, useEffect } from 'react'
+import Header from './Header/Header'
+import Intro from '../components/Intro'
+import './Home.css'
+import Fromfavbarnd from '../components/Fromfavbarnd'
+import ShopByCat from '../components/ShopByCat'
+import Footer from '../components/Footer'
+import Slider from '../components/slider/Slider'
+import MartkettingBanner from '../components/MartkettingBanner'
+import PopularProducts from '../components/PopularProducts'
+import { appDispatch } from 'src/store'
+import { useDispatch } from 'react-redux'
+import { fetchProducts } from 'src/store/actions/productAction'
+import { fetchCategories } from 'src/store/actions/categoriesActions'
+
+export default function Home() {
+  const dispatch: appDispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(fetchProducts())
+    dispatch(fetchCategories())
+  }, [dispatch])
+
+  return (
+    <Fragment>
+      <Header />
+      <Slider />
+      <div
+        css={css`
+          width: 70rem;
+          margin: auto;
+        `}
+      >
+        <Intro />
+        <Fromfavbarnd />
+        <ShopByCat />
+        <MartkettingBanner />
+        <PopularProducts />
+      </div>
+      <Footer />
+    </Fragment>
+  )
+}
