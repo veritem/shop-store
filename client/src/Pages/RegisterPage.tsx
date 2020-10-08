d9c94fae043bed7764e13df53ed7e2bd3022111b
Makuza Mugabo Verite <mugaboverite@gmail.com>
2020-10-08 17:47:36 +0200

    fixed css for registration page

---
+++ client/src/Pages/RegisterPage.tsx
@@ -0,0 +1,22 @@
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core'
+import Register from '../../components/auth/Register/Register'
+import './RegisterPage.css'
+
+function RegisterPage() {
+  return (
+    <div
+      css={css`
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      `}
+    >
+      <Register />
+    </div>
+  )
+}
+
+export default RegisterPage
