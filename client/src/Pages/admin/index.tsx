0ec32d01568489655204110f2b165bb7936555a0
Makuza Mugabo Verite <mugaboverite@gmail.com>
2020-11-24 18:27:04 +0200

    added layouts to make code more resuable

--- client/src/Pages/admin/index.tsx
+++ client/src/Pages/admin/index.tsx
@@ -1,6 +1,29 @@
+/** @jsxImportSource @emotion/core */
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { css, jsx } from '@emotion/core'
 import React from 'react'
 
 export const index = (): React.ReactElement => {
-  return <div>Hello from admin</div>
+  return (
+    <div
+      css={css`
+        display: grid;
+        grid-template-columns: 1fr 3fr;
+      `}
+    >
+      <div
+        css={css`
+          background-color: red;
+          height: 100vh;
+        `}
+      >
+        <p>Side bar</p>
+      </div>
+      <div>
+        <p>Main body</p>
+      </div>
+    </div>
+  )
 }
 export default index
