043a532f3c1fb1e5a1f606f61f60fa4137f8b7de
Makuza Mugabo Verite <mugaboverite@gmail.com>
2020-11-24 20:23:06 +0200

    added three cards

--- client/src/Pages/Login.tsx
+++ client/src/Pages/Login.tsx
@@ -1,8 +1,6 @@
 /** @jsxImportSource @emotion/core */
-
-
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import {css,jsx} from '@emotion/core'
+import { css, jsx } from '@emotion/core'
 import { useDispatch } from 'react-redux'
 import { useState } from 'react'
 import { loadUser, login } from '../store/actions/authActions'
--- client/src/Pages/admin/index.tsx
+++ client/src/Pages/admin/index.tsx
@@ -7,7 +7,40 @@ import AdminLayout from 'src/layout/AdminLayout'
 export const index = (): React.ReactElement => {
   return (
     <AdminLayout>
-      <p>Hello</p>
+      <div
+        css={css`
+          padding: 10px 20px;
+          display: grid;
+          grid-template-columns: repeat(3, 1fr);
+          gap: 20px;
+          div {
+            height: 150px;
+            border-radius: 0.5rem;
+          }
+        `}
+      >
+        <div
+          css={css`
+            background-color: #3f47f4;
+          `}
+        >
+          <h3>120</h3> users
+        </div>
+        <div
+          css={css`
+            background-color: #f3b82a;
+          `}
+        >
+          <h3>2040</h3> Orders
+        </div>
+        <div
+          css={css`
+            background-color: #1ce2b1;
+          `}
+        >
+          <h3>30</h3> Products
+        </div>
+      </div>
     </AdminLayout>
   )
 }
