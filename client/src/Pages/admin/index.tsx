a39c7258361697162097da40cb1d4304f1224dcd
Makuza Mugabo Verite <mugaboverite@gmail.com>
2020-12-04 17:23:23 +0200

    added user's table

--- client/src/Pages/admin/index.tsx
+++ client/src/Pages/admin/index.tsx
@@ -2,54 +2,58 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { css, jsx } from '@emotion/core'
 import React from 'react'
+import UserTable from 'src/components/UserTable'
 import AdminLayout from 'src/layout/AdminLayout'
 
 export const index = (): React.ReactElement => {
   return (
     <AdminLayout>
-      <div
-        css={css`
-          padding: 10px 20px;
-          display: grid;
-          grid-template-columns: repeat(3, 1fr);
-          gap: 20px;
-          div {
-            height: 150px;
-            border-radius: 0.5rem;
-            text-align: right;
-            padding: 1rem;
-            h3 {
-              font-size: 3rem;
-              color: #dede;
+      <>
+        <div
+          css={css`
+            padding: 10px 20px;
+            display: grid;
+            grid-template-columns: repeat(3, 1fr);
+            gap: 20px;
+            div {
+              height: 150px;
+              border-radius: 0.5rem;
+              text-align: right;
+              padding: 1rem;
+              h3 {
+                font-size: 3rem;
+                color: #dede;
+              }
+              p {
+                color: #fefefe;
+              }
             }
-            p {
-              color: #fefefe;
-            }
-          }
-        `}
-      >
-        <div
-          css={css`
-            background-color: #d90429;
           `}
         >
-          <h3>120</h3> <p>users</p>
+          <div
+            css={css`
+              background-color: #d90429;
+            `}
+          >
+            <h3>120</h3> <p>users</p>
+          </div>
+          <div
+            css={css`
+              background-color: #8d99ae;
+            `}
+          >
+            <h3>2040</h3> <p>Orders</p>
+          </div>
+          <div
+            css={css`
+              background-color: #2b2d42;
+            `}
+          >
+            <h3>30</h3> <p>Products</p>
+          </div>
         </div>
-        <div
-          css={css`
-            background-color: #8d99ae;
-          `}
-        >
-          <h3>2040</h3> <p>Orders</p>
-        </div>
-        <div
-          css={css`
-            background-color: #2b2d42;
-          `}
-        >
-          <h3>30</h3> <p>Products</p>
-        </div>
-      </div>
+        <UserTable />
+      </>
     </AdminLayout>
   )
 }
---
+++ client/src/components/UserTable.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UserTable() {
+  return (
+    <div>
+      <h1>Users here</h1>
+    </div>
+  )
+}
+
+export default UserTable
